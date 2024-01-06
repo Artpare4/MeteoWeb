242617f0d93f6cbb38540fb60dbc910af7e0c4d7
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-01-06 22:48:01 +0100

    Suppression du bouton agree terms du formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -40,14 +40,6 @@ class RegistrationFormType extends AbstractType
                         'maxMessage'=> 'Votre email est trop long, il doit faire au maximum {{ limit }} caractères maximum'
                     ])
                 ]])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
