669cda1944b18faf97aec5dd8516f2e1b7d395df
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-01-08 22:54:07 +0100

    Ajout de l'attribut 'client' à la classe indexController et création du constructeur prenant en paramètre un HttpClientInterface

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,9 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class IndexController extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client=$client;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
