d8ba3a7c7a24e97f220977afc9be25a438380779
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-01-10 19:13:20 +0100

    Ajout de la requête de l'api pour les adresses

--- src/Controller/AdresseController.php
+++ src/Controller/AdresseController.php
@@ -3,16 +3,63 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AdresseController extends AbstractController
+
 {
-    #[Route('/search', name: 'app_adresse')]
-    public function index(): Response
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
     {
+        $this->client=$client;
+    }
+    #[Route('/search', name: 'app_adresse')]
+    public function index(Request $request): Response
+    {
+        $q='';
+        $rue=$request->query->get('rue');
+        if($rue != null){
+            $rue= str_replace(' ','%20',$rue);
+            $q=$q.$rue;
+        }
+        else{
+            $rue='';
+        }
+
+        $ville=$request->query->get('ville');
+        if($ville == null){
+            $ville='';
+        }
+        else{
+            $q=$q.'%20'.$ville;
+        }
+
+        $cp=$request->query->get('cp');
+        if($cp == null){
+            $cp='';
+        }
+        else{
+            $q=$q.'%20'.$cp;
+        }
+
+
+        if($q!=''){
+            $res=$this->client->request(
+                'GET',
+                "https://api-adresse.data.gouv.fr/search/?q=$q&type=housenumber&autocomplete=1")->toArray();
+            var_dump($res);
+        }
+        else{
+            $res=null;
+        }
+
         return $this->render('adresse/index.html.twig', [
             'controller_name' => 'AdresseController',
+            'adresses'=>$res
         ]);
     }
 }
