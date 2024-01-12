4f0462aacd120a6c46ef076ac8d9816901da1ee0
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-01-12 18:52:01 +0100

    Modification du CRUD de User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use PharIo\Manifest\Email;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +19,16 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname','Prénom'),
+            TextField::new('lastname','Nom'),
+            EmailField::new('email','Email'),
+            ArrayField::new('roles','Rôles'),
         ];
     }
-    */
+
 }
